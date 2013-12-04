01bc6dad684c2e903f082c7def477c74d917897a
Mike Dawson <mike@toughra.com>
2013-12-04 14:22:21 +0400

    Update Android build script to include icons correctly in Cordova

--- setup/android/setup-android.sh
+++ setup/android/setup-android.sh
@@ -9,53 +9,75 @@ TARGETDIR=""
 SRCDIR="../../"
 
 WORKINGDIR=$(pwd)
+TARGETDIR="./build"
 
-#Make a new build directory
-if [ "$1" == "build" ]; then
-
-    if [ "$2" == "" ]; then
-        TARGETDIR="./build"
-    else 
-        TARGETDIR=$1
-    fi
-
-    if [ ! -d $TARGETDIR ]; then
-        mkdir $TARGETDIR
-    fi
-
-    cd $TARGETDIR
-    cordova create ustadmobile com.toughra.ustadmobile UstadMobile
-    cd ustadmobile
-    cordova platform add android
-    cordova plugin add org.apache.cordova.device
-    cordova plugin add org.apache.cordova.network-information
-    cordova plugin add org.apache.cordova.battery-status
-    cordova plugin add org.apache.cordova.device-motion
-    cordova plugin add org.apache.cordova.device-orientation
-    cordova plugin add org.apache.cordova.file
-    cordova plugin add org.apache.cordova.file-transfer
-    cordova plugin add org.apache.cordova.globalization
-    cordova plugin add org.apache.cordova.splashscreen
-    cordova plugin add org.apache.cordova.console
-
-    echo "Made a cordova project in $TARGETDIR/ustadmobile"
-
-    cd $WORKINGDIR
-    cd $SRCDIR
-    
-    FILEDEST=$WORKINGDIR/$TARGETDIR/ustadmobile/www
-
-    # we don't want this file - will confuse cordova
-    if [ -e spec.html ]; then
-        rm spec.html
-    fi
-    
-    cp -r *.html img js jqm res $FILEDEST
-    cp css/index.css css/jquery.mobile-1.3.2.min.css css/qunit-1.12.0.css $FILEDEST/css
-    
-    echo "Done - now cd into $TARGETDIR/ustadmobile and run"
-    echo "cordova build ; cordova compile ; cordova run "
+#clean
+if [ -d $TARGETDIR ]; then
+    echo "deleting (cleaning) old build dir"
+    rm -rf $TARGETDIR 
 fi
+     
+if [ ! -d $TARGETDIR ]; then
+    mkdir $TARGETDIR
+fi
+
+cd $TARGETDIR
+cordova create ustadmobile com.toughra.ustadmobile UstadMobile
+cd ustadmobile
+cordova platform add android
+cordova plugin add org.apache.cordova.device
+cordova plugin add org.apache.cordova.network-information
+cordova plugin add org.apache.cordova.battery-status
+cordova plugin add org.apache.cordova.device-motion
+cordova plugin add org.apache.cordova.device-orientation
+cordova plugin add org.apache.cordova.file
+cordova plugin add org.apache.cordova.file-transfer
+cordova plugin add org.apache.cordova.globalization
+cordova plugin add org.apache.cordova.splashscreen
+cordova plugin add org.apache.cordova.console
+
+echo "Made a cordova project in $TARGETDIR/ustadmobile"
+
+cd $WORKINGDIR
+cd $SRCDIR
+
+FILEDEST=$WORKINGDIR/$TARGETDIR/ustadmobile/www
+
+# we don't want this file - will confuse cordova
+if [ -e spec.html ]; then
+    rm spec.html
+fi
+
+cp -r *.html img js jqm res $FILEDEST
+cp css/index.css css/jquery.mobile-1.3.2.min.css css/qunit-1.12.0.css $FILEDEST/css
+
+
+echo "Done - now cd into $TARGETDIR/ustadmobile and run"
+
+
+#copy icon where it should go - strangely this does not happen by default
+RESLIST="hdpi ldpi mdpi xhdpi"
+
+for res in $RESLIST; do
+    cp res/icon/android/icon-??-$res.png $FILEDEST/../platforms/android/res/drawable-$res/icon.png
+done
+cp res/icon/android/icon-96-xhdpi.png $FILEDEST/../platforms/android/res/drawable/icon.png
+
+
+cd $WORKINGDIR
+cd $TARGETDIR/ustadmobile
+cordova build
+
+
+if [ "$1" == "run" ]; then
+    cordova run
+fi
+
+if [ "$1" == "emulate" ]; then
+    cordova emulate
+fi
+
+
 
 
 
