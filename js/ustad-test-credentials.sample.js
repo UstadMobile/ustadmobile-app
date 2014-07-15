ede70e40f782df43e3528668b586a2415a6d6902
Mike Dawson <mike@toughra.com>
2014-07-16 01:31:12 +0400

    Modify sample credentials

--- js/ustad-test-credentials.sample.js
+++ js/ustad-test-credentials.sample.js
@@ -2,7 +2,7 @@
 /**
  * Use these to actually conduct the tests
  */
-var validUsername = "varunasingh";
-var validPassword = "varuna";
+var validUsername = "username";
+var validPassword = "password";
 
 
