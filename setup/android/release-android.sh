84460c7828999f2a6c572af5a2c01d2e9a586a3f
Mike Dawson <mike@toughra.com>
2014-07-31 18:01:04 +0200

    Update Android release procedure script for new cordova

--- setup/android/release-android.sh
+++ setup/android/release-android.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-source release-settings-android.sh
+if [ ! -e release-ant.properties ]; then
+    echo "You must set signing credentials - please see release-ant.properties.example and copy to release-ant.properties"
+    exit 1
+fi
+
+if [ ! -e build/ustadmobile ]; then
+    echo "You must build using setup-android first - then ask for release"
+    exit 1
+fi
+
+#source release-settings-android.sh
 source ../../ustad_version
 
-echo "WARNING: As part of release process android:debuggable will be set to false."
-echo "Please run setup-android.sh again to regenerate"
 
-echo KEYSTORE = $KEYSTORE
+cp release-ant.properties build/ustadmobile/platforms/android/ant.properties
 
 cd build/ustadmobile
 
-#Sets Debuggable to false for Android
-sed -i -e 's/android\:debuggable=\"true\"/android:debuggable=\"false\"/' ./platforms/android/AndroidManifest.xml
-
 #Updates the version
-sed -i -e s/android:versionCode=\"1\"/android:versionCode=\"$VERSIONCODE\"/g ./platforms/android/AndroidManifest.xml
+sed -i -e s/version=\"0.0.1\"/version=\"$VERSION\"\ android-versionCode=\"$VERSIONCODE\"/g ./config.xml
 
 #Builds the apk
 cordova build --release
 
-#Signs the apk
-jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore $KEYSTORE ./platforms/android/bin/UstadMobile-release-unsigned.apk $KEYALIAS
-
-#Zip Aligns the apk
-zipalign -v 4 ./platforms/android/bin/UstadMobile-release-unsigned.apk ./platforms/android/bin/UstadMobile-release-aligned.apk
-
-echo "Wrote signed/zip aligned file to: ./build/ustadmobile/platforms/android/bin/UstadMobile-release-aligned.apk"
 
