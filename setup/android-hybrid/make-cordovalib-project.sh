924558c1a7de8106860fe75af9bad54765724d48
Mike Dawson <mike@toughra.com>
2015-01-18 19:38:21 +0100

    Remove duplicate android project creation

--- setup/android-hybrid/make-cordovalib-project.sh
+++ setup/android-hybrid/make-cordovalib-project.sh
@@ -2,7 +2,6 @@
 
 #Make a blank Cordova Lib project so our app can use it as a lib
 
-android create project --target 6 --name UstadMobileActivity --path ./ustadmobileandroid --activity UstadMobileActivity --package com.toughra.ustadmobile
 
 cordova create umcordovalib com.ustadmobile UMCordovaLib
 cd umcordovalib
